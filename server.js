155a8c88dab24cce00a090dacc788cbaaa9ccb89
Moppy <erik@eext.us>
2016-03-19 18:18:23 +0900

    Initalize repo

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+// Control + C stops the server in the console
+// __dirname is a built in variable that give the full file path
+
+// / is the root url
+// req includes all infro from user
+// res is used to send data back
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, resp, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}
+
+// Middleware called on every request
+app.use(middleware.logger);
+
+// Create an about page. Middleware only called on this path
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About us');
+})
+
+app.use(express.static(__dirname+ '/public'));
+
+
+// 3000 usually isn't used so it won't cause any conflicts with system trash
+// The second argument, a function, gets called when the server starts up
+// Using all upper case indicates that the variable is meant to be a constant, like let in swift.
+var PORT = 3000;
+
+app.listen(PORT, function() {
+	console.log('Server started up on port ' + PORT);
+});
